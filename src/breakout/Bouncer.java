d364a72894c713d18da92f3247faf66e73f817c8
Eric Doppelt <ead45@duke.edu>
2020-01-17 13:48:33 -0500

    Have ball bouncing on screen

--- src/breakout/Bouncer.java
+++ src/breakout/Bouncer.java
@@ -4,14 +4,57 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 
 public class Bouncer {
-    private Circle myBouncer;
+    private Circle myCircle;
+    private int myDirectionX;
+    private int myDirectionY;
+    private int mySpeedX;
+    private int mySpeedY;
 
-    public Bouncer(int centerX, int centerY, int radius, Paint fill) {
-        myBouncer = new Circle(centerX, centerY, radius);
-        myBouncer.setFill(fill);
+
+    public Bouncer(int centerX, int centerY, int radius, Paint fill,  int speedX, int speedY) {
+        myCircle = new Circle(centerX, centerY, radius);
+        myCircle.setFill(fill);
+        myDirectionX = 1;
+        myDirectionY = 1;
+        mySpeedX = speedX;
+        mySpeedY = speedY;
+}
+
+    public Circle getCircle() {
+        return myCircle;
     }
 
-    public Circle getBouncer() {
-        return myBouncer;
+    public int getXChange() {
+        return myDirectionX * mySpeedX;
+    }
+
+    public int getYChange() {
+        return myDirectionY * mySpeedY;
+    }
+
+    public void setX(double x) {
+        myCircle.setCenterX(x);
+    }
+
+    public void setY(double y) {
+        myCircle.setCenterY(y);
+    }
+
+    public boolean xOnScreen() {
+        if (myCircle.getCenterX() + myCircle.getRadius() >= Main.SIZE_X || myCircle.getCenterX() - myCircle.getRadius() <= 0) return false;
+        return true;
+    }
+
+    public boolean yOnScreen() {
+        if (myCircle.getCenterY() + myCircle.getRadius() >= Main.SIZE_Y || myCircle.getCenterY() - myCircle.getRadius() <= 0) return false;
+        return true;
+    }
+
+    public void reverseXDirection() {
+        myDirectionX *= -1;
+    }
+
+    public void reverseYDirection() {
+        myDirectionY *= -1;
     }
 }
--- src/breakout/Main.java
+++ src/breakout/Main.java
@@ -1,15 +1,18 @@
 package breakout;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
-import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+
+import java.util.ArrayList;
+
 
 public class Main extends Application {
 
@@ -21,13 +24,23 @@ public class Main extends Application {
     public static final int PADDLE_WIDTH = 100;
     public static final int PADDLE_HEIGHT = 20;
     public static final int BOUNCER_RADIUS = 10;
+    public static final int BOUNCER_SPEED_X = 500;
+    public static final int BOUNCER_SPEED_Y = 600;
     public static final Paint BOUNCER_COLOR = Color.ROYALBLUE;
+
+    public static final int FRAMES_PER_SECOND = 60;
+    public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+    public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
 
     private static final int MOVER_SPEED = 20;
 
     private Scene myScene;
-    private Bouncer mainBouncer;
-    private Paddle mainPaddle;
+    private Scene mySplashScreen;
+
+    private Bouncer myBouncer;
+    private ArrayList<Bouncer> allBouncers = new ArrayList<>();
+    private Paddle myPaddle;
+
     /**
      * Start of the program.
      */
@@ -43,15 +56,35 @@ public class Main extends Application {
         stage.setTitle(TITLE);
         stage.show();
 
+        KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY));
+        Timeline animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(frame);
+        animation.play();
     }
-    
+
+
+    private void step(double elapsedTime) {
+
+        for (Bouncer tempBouncer : allBouncers) {
+            if (!tempBouncer.xOnScreen()) tempBouncer.reverseXDirection();
+            tempBouncer.setX(tempBouncer.getCircle().getCenterX() + tempBouncer.getXChange() * elapsedTime);
+            System.out.println((tempBouncer.getXChange()));
+            if (!tempBouncer.yOnScreen()) tempBouncer.reverseYDirection();
+            tempBouncer.setY(tempBouncer.getCircle().getCenterY() + tempBouncer.getYChange() * elapsedTime);
+
+        }
+    }
+
     private Scene setupGame(int width, int height, Paint background) {
         Group root = new Group();
-        mainBouncer = new Bouncer(width / 2, height - BOUNCER_RADIUS - PADDLE_HEIGHT, BOUNCER_RADIUS, BOUNCER_COLOR);
-        mainPaddle = new Paddle((width - PADDLE_WIDTH) / 2, height - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_COLOR);
+        myBouncer = new Bouncer(width / 2, height - BOUNCER_RADIUS - PADDLE_HEIGHT, BOUNCER_RADIUS, BOUNCER_COLOR, BOUNCER_SPEED_X, BOUNCER_SPEED_Y);
+        System.out.println("okay");
+        allBouncers.add(myBouncer);
+        myPaddle = new Paddle((width - PADDLE_WIDTH) / 2, height - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_COLOR);
 
-        root.getChildren().add(mainBouncer.getBouncer());
-        root.getChildren().add(mainPaddle.getPaddle());
+        root.getChildren().add(myBouncer.getCircle());
+        root.getChildren().add(myPaddle.getPaddle());
 
         Scene scene = new Scene(root, width, height, background);
 
@@ -60,12 +93,13 @@ public class Main extends Application {
         return scene;
     }
 
+
     private void handleKeyInput(KeyCode code) {
         if (code == KeyCode.RIGHT) {
-            mainPaddle.getPaddle().setX(mainPaddle.getPaddle().getX() + MOVER_SPEED);
+            myPaddle.getPaddle().setX(myPaddle.getPaddle().getX() + MOVER_SPEED);
         }
         else if (code == KeyCode.LEFT) {
-            mainPaddle.getPaddle().setX(mainPaddle.getPaddle().getX() - MOVER_SPEED);
+            myPaddle.getPaddle().setX(myPaddle.getPaddle().getX() - MOVER_SPEED);
         }
     }
 }
